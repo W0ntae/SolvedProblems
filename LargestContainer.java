45268561041b41dcede4949949d096ddd5ec9497
w0ntae <bitcampcoding@gmail.com>
2022-07-12 12:39:49 +0900

    Solved

---
+++ LargestContainer.java
@@ -0,0 +1,24 @@
+class LargestContainer {
+  public int maxArea(int[] height) {
+    int maxArea = 0, left = 0, right = height.length -1;
+    while (left < right){
+      if (height[left] < height[right]){
+        int length = right - left;
+        int area = length * height[left];
+        if (area > maxArea){
+          maxArea = area;
+        }
+        ++left;
+      }
+      else if (height[left] >= height[right]){
+        int length = right - left;
+        int area = length * height[right];
+        if (area > maxArea){
+          maxArea = area;
+        }
+        --right;
+      }
+    }
+    return maxArea;
+  }
+}
